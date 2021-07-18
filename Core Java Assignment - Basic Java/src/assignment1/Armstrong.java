4b87b6dcf06d49b0a3b638d88d457b48a3353eb5
avinash210399 <iamavinashkumar21@gmail.com>
2021-07-18 15:34:23 +0530

    Comitted 1st program

--- Core Java Assignment - Basic Java/src/assignment1/Armstrong.java
+++ Core Java Assignment - Basic Java/src/assignment1/Armstrong.java
@@ -1,9 +1,29 @@
 package assignment1;
 
-public class Armstrong {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		
-	}
+class Armstrong {
+    public boolean armstrongCheck(int num) {
+        int sum=0,arm = num;
+        while(num>0) {
+            int temp = num%10;
+            sum+=temp*temp*temp;
+            num = num/10;
+        };
+        if(sum == arm) {
+            return true;
+        }
+        return false;
+    }
+}
 
+ class AssignmentQ1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        Armstrong check = new Armstrong();
+        boolean res = check.armstrongCheck(num);
+        System.out.println(res);
+        scan.close();
+    }
 }
