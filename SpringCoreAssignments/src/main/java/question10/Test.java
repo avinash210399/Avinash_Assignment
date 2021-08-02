eaa0b549c4a2d22b987be14aaf78a7d12c8d685c
avinash210399 <iamavinashkumar21@gmail.com>
2021-08-02 17:09:20 +0530

    Committing 10th Question

---
+++ SpringCoreAssignments/src/main/java/question10/Test.java
@@ -0,0 +1,15 @@
+package question10;
+
+public class Test {
+	private int Questionid;
+
+	public int getQuestionid() {
+		return Questionid;
+	}
+
+	public void setQuestionid(int questionid) {
+		this.Questionid = questionid;
+	}
+	
+
+}
---
+++ SpringCoreAssignments/src/main/java/question10/Tsetclass.java
@@ -0,0 +1,13 @@
+package question10;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Tsetclass {
+
+	public static void main(String[] args)
+	{
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		
+	}
+}
