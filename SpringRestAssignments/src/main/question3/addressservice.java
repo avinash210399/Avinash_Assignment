8f30ecb1007daa9454a5b56e002c4be0e9f44823
avinash210399 <iamavinashkumar21@gmail.com>
2021-08-02 19:45:46 +0530

    Committing Rest Assignments

---
+++ SpringRestAssignments/src/main/question3/addressservice.java
@@ -0,0 +1,8 @@
+package com.example.demo.Q3;
+
+import java.util.List;
+
+public interface addressservice {
+	public Address predict(String p);
+
+}
---
+++ SpringRestAssignments/src/main/question4/validationInteface.java
@@ -0,0 +1,6 @@
+package com.example.demo.Q4;
+
+public interface validationInteface {
+	public String check(String num,String c);
+
+}
